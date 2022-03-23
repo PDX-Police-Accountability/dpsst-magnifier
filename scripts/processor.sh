6287eb14b5b30a6ad0c7592532099611055e15a1
Marc Poris <marc.poris@gmail.com>
2022-03-23 16:21:15 -0700

    Added script to run both markdown and yaml processors.

---
+++ scripts/processor.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+process_directory() {
+  date_string=$1
+  data_project_dir="../dpsst-data"
+  input_dir="../dpsst-scraper/scraped-data/$date_string"
+  yaml_output_dir="$data_project_dir/officers/yaml"
+  markdown_output_dir="$data_project_dir/officers/markdown"
+
+  bin/rake dpsst:yamlize_directory[$input_dir,$date_string,$yaml_output_dir]
+  bin/rake dpsst:markdown_directory[$input_dir,$date_string,$markdown_output_dir]
+
+  pushd $data_project_dir
+  git add .
+  git commit -m "Processed on $date_string."
+  git tag "processed-$date_string"
+  popd
+}
+
+if [ $# -ne 1 ]; then
+  echo "Usage: $0 date-string"
+else
+  echo "Processing: $1"
+  process_directory $1
+fi
