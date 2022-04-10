6955e25e51b2fec5fed222be7fb40560f597e59a
Marc Poris <marc.poris@gmail.com>
2022-04-10 04:10:24 -0700

    Create multiple summaries - each sorted by a different column.

--- app/services/dpsst_services/markdown_table.rb
+++ app/services/dpsst_services/markdown_table.rb
@@ -1,8 +1,20 @@
 module DpsstServices::MarkdownTable
 
-  def table_header(columns)
-    first_row = '| ' + columns.join(' | ') + ' |'
-    second_row = first_row.gsub(/[\w]/, '-')
+  def table_header(columns, column_header_transformation)
+    cols = columns.map do |col|
+      if column_header_transformation
+        self.send(column_header_transformation, col)
+      else
+        col.to_s
+      end
+    end
+
+    dashes = columns.map do |col|
+      ''.ljust(col.length, '-')
+    end
+
+    first_row = '| ' + cols.join(' | ') + ' |'
+    second_row = '| ' + dashes.join(' | ') + ' |'
 
     first_row + "\n" + second_row + "\n"
   end
@@ -11,9 +23,9 @@ module DpsstServices::MarkdownTable
     '| ' + cells.join(' | ') + ' |' + "\n"
   end
 
-  def array_to_table_markdown(title, cols, a)
+  def array_to_table_markdown(title, cols, a, column_header_transformation = nil)
     t = "## #{title}" + "\n"
-    th = table_header(cols.map(&:to_s))
+    th = table_header(cols, column_header_transformation)
 
     a.each_with_object(t + th) do |h, md|
       md << table_row(h.slice(*cols).values)
