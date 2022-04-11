578c38e0e499e1da529520934f42a91e013adcc1
Marc Poris <marc.poris@gmail.com>
2022-04-10 22:18:39 -0700

    Run the training summarizer during processing.

--- scripts/processor.sh
+++ scripts/processor.sh
@@ -11,6 +11,7 @@ process_directory() {
   bin/rake dpsst:yamlize_directory[$input_dir,$date_string,$yaml_dir]
   bin/rake dpsst:markdown_directory[$input_dir,$date_string,$markdown_dir]
   bin/rake dpsst:summarize_directories[$date_string,$yaml_dir,$summary_dir]
+  bin/rake dpsst:summarize_training[$date_string,$yaml_dir,$summary_dir]
 
   pushd $data_project_dir
   git add .
